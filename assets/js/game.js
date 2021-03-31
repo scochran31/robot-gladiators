71bafc3ed3713d1208f140a2b20c49e27789192a
Shawn Cochran <shawn.cochran31@gmail.com>
2021-03-30 18:53:09 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+var playerName = window.prompt("What is your robot's name");
+
+console.log(playerName)
+
+console.log(10+10)
+
+console.log("Our robot's name is" + playerName)
+
+function fight () {
+    window.alert('The fight has begun!');
+}
+
+fight();
